69e0fcdd0241aff486339d567b453d2e908c5043
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 17:38:33 -0600

    Proyecto No. 21

--- Ejercicio 21/Ejercicio 21/Program.cs
+++ Ejercicio 21/Ejercicio 21/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Ingrese la cantidad de numeros que quiera: ");
+Console.WriteLine("");
+int numero = int.Parse(Console.ReadLine());
+int cantidad = 0;
+int numeros = 0;
+for (int i = 1; i <= numero; i++)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    Console.WriteLine("");
+    numeros = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    if (numeros < 0)
+    {
+        cantidad = cantidad + 1;
+    }
+}
+
+Console.WriteLine($"Los cantidad de números negativos son {cantidad}");
