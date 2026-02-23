14ba1a31ab8065fd6276634fde86b824c9a265d9
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 16:06:25 -0600

    Proyecto No. 15

--- Ejercicio 15/Ejercicio 15/Program.cs
+++ Ejercicio 15/Ejercicio 15/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int numero = 0;
+int x = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Ingrese el {i}° número: ");
+    Console.WriteLine("");
+    numero = int.Parse(Console.ReadLine()) + numero;
+    Console.WriteLine("");
+    x = i;
+}
+Console.WriteLine($"El promedio es {numero/x}");
