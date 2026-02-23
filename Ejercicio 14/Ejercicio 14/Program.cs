841939b0e3b46f36ecb85f3438351a6c5e4954e3
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 15:59:56 -0600

    Proyecto No. 14

--- Ejercicio 14/Ejercicio 14/Program.cs
+++ Ejercicio 14/Ejercicio 14/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int numero = 0;
+for (int i = 1; i <= 3; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número : ");
+    Console.WriteLine("");
+    numero= int.Parse(Console.ReadLine()) + numero;
+    Console.WriteLine("");
+}
+Console.WriteLine($"El total es {numero}");
