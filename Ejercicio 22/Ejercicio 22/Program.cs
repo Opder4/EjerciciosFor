0b9e14a9564db1bd2dc4b8a80f7e03efc94b2d23
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 17:46:34 -0600

    Proyecto No. 22

--- Ejercicio 22/Ejercicio 22/Program.cs
+++ Ejercicio 22/Ejercicio 22/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Ingrese la cantidad de ventas que ha tenido: ");
+Console.WriteLine("");
+int cantidadventas=int.Parse(Console.ReadLine());
+double totalventas = 0;
+for (int i=1; i<=cantidadventas; i++)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"Ingrese el monto de la {i}° venta: ");
+    Console.WriteLine("");
+    double ventas = double.Parse(Console.ReadLine());
+    totalventas = totalventas + ventas;
+}
+Console.WriteLine("");
+Console.WriteLine($"El total de las ventas es de Q{totalventas}");
