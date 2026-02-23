766eada0c615385bc305ceea149c33768e2dadf8
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 17:55:35 -0600

    Proyecto No. 24

--- Ejercicio 24/Ejercicio 24/Program.cs
+++ Ejercicio 24/Ejercicio 24/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System.Timers;
+
+Console.WriteLine("Ingresa la cantidad total de notas de notas a ingresar: ");
+Console.WriteLine("");
+int cantidadnotas = int.Parse(Console.ReadLine());
+double totalnotas = 0;
+double notafinal = 0;
+int x = 0;
+for (int i = 1; i <= cantidadnotas; i++)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"Ingrese la {i}° nota: ");
+    Console.WriteLine("");
+    notafinal = double.Parse(Console.ReadLine());
+    totalnotas = totalnotas + notafinal;
+    x = i;
+}
+if ((totalnotas / x) >= 61)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"El promedio final es de {totalnotas / x}");
+    Console.WriteLine("");
+    Console.WriteLine("El alumno está Aprobado");
+}
+else
+{
+    Console.WriteLine("");
+    Console.WriteLine($"El promedio final es de {totalnotas / x}");
+    Console.WriteLine("");
+    Console.WriteLine("El alumno está Desaprobado");
+}
