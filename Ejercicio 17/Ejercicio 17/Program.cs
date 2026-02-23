a3e863eb3781e6c47a2a5c978140f876d12d2ea8
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 16:37:31 -0600

    Proyecto No. 17

--- Ejercicio 17/Ejercicio 17/Program.cs
+++ Ejercicio 17/Ejercicio 17/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int numeros = 0;
+
+for (int i = 1; i<=5; i++)
+{
+    Console.WriteLine($"Ingrese el {i}° número: ");
+    Console.WriteLine("");
+    numeros = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+}
+int cantidadpar = 0;
+for (int z=1; z<=5; z++)
+{
+    if (numeros%2==0)
+    {
+        cantidadpar = z;
+    }
+}
+Console.WriteLine($"la cantidad que son pares son: {cantidadpar}");
