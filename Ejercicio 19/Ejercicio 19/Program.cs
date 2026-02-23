dad309f9961953c66d670cd802f4e1c6bc0910ad
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 17:15:36 -0600

    Proyecto No. 19

--- Ejercicio 19/Ejercicio 19/Program.cs
+++ Ejercicio 19/Ejercicio 19/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int numeros = 0;
+int menor = int.MaxValue;
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    Console.WriteLine("");
+    numeros = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    if (numeros < menor)
+    {
+        menor = numeros;
+    }
+}
+Console.WriteLine($"{menor}");
