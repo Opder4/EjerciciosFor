780bdb4cbf0425fcd0fba74d77cc5208f56ccb80
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 16:31:39 -0600

    Proyecto No. 16

--- Ejercicio 16/Ejercicio 16/Program.cs
+++ Ejercicio 16/Ejercicio 16/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int numero = 0;
+
+    for (int i=1; i<=5; i++)
+{
+    Console.WriteLine($"Ingrese el {i}° número: ");
+    Console.WriteLine("");
+    numero=int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+}
+int cantidad = 0;
+    for (int z=1; z<=5; z++)
+{
+    if (numero>10)
+    {
+        cantidad = z;
+    }
+}
+Console.WriteLine($"Los que cumplen son mayor a 10 son: {cantidad}");
+
+
