d1a3f160447f87587eb7e783cc1b7bed677a20ea
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 17:12:09 -0600

    Proyecto No. 18 Tuve que buscar como realizarlo, pero ya lo entendí

--- Ejercicio 18/Ejercicio 18/Program.cs
+++ Ejercicio 18/Ejercicio 18/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int numeros = 0;
+int mayor = int.MinValue;
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Ingrese su {i}° número: ");
+    Console.WriteLine("");
+    numeros = int.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    if (numeros>mayor)
+    {
+        mayor=numeros;
+    }
+}
+Console.WriteLine($"{mayor}");
+
+
+
