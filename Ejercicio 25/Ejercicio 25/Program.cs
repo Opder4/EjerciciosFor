c4e4f656f5f05e4a95c0f8ed1e30efad7cf6a4c7
Emanuel Ramírez <er6348100@gmail.com>
2026-02-23 18:14:11 -0600

    Proyecto No. 25

--- Ejercicio 25/Ejercicio 25/Program.cs
+++ Ejercicio 25/Ejercicio 25/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Ingrese un número: ");
+Console.WriteLine("");
+int numero=int.Parse(Console.ReadLine());
+string salida = "";
+for (int i=1;  i<=numero; i++)
+{
+    salida = salida + "*";
+    Console.WriteLine(salida);
+}
